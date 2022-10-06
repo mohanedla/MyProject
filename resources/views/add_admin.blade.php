c9222dbbc38c02fc182fc719b271331deee20f30
mohanedla <mohanedla007700@gmail.com>
2022-10-06 02:53:00 +0200

    update 8.1

--- resources/views/add_admin.blade.php
+++ resources/views/add_admin.blade.php
@@ -229,7 +229,102 @@
                 </div>
                   </div>
             </div>
-
+            <div class="col-sm-8 col-lg-9 mtb_20">
+                <!-- contact  -->
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3">
+                        <div class="panel-login panel">
+                            <div class="panel-heading">
+                                <div class="row mb_20">
+                                    <div class="col-xs-6">
+                                        <a href="#" class="active"
+                                            id="login-form-link">{{ __('Login') }}</a>
+                                    </div>
+                                    <div class="col-xs-6">
+                                        <a href="#" id="register-form-link">{{ __('Register') }}</a>
+                                    </div>
+                                </div>
+                                <hr>
+                            </div>
+                            <div class="panel-body">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <form id="login-form" action="#" method="post">
+                                            <div class="form-group">
+                                                <input type="text" name="username" id="username1"
+                                                    tabindex="1" class="form-control"
+                                                    placeholder="{{ __('Username') }}" value="">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" name="password" id="password"
+                                                    tabindex="2" class="form-control"
+                                                    placeholder="{{ __('Password') }}">
+                                            </div>
+                                            <div class="form-group text-center">
+                                                <input type="checkbox" tabindex="3" class=""
+                                                    name="remember" id="remember">
+                                                <label for="remember"> {{ __('Remember Me') }}</label>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="row">
+                                                    <div class="col-sm-6 col-sm-offset-3">
+                                                        <input type="submit" name="login-submit"
+                                                            id="login-submit" tabindex="4"
+                                                            class="form-control btn btn-login"
+                                                            value="{{ __('Login') }}">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="row">
+                                                    <div class="col-lg-12">
+                                                        <div class="text-center">
+                                                            <a href="#" tabindex="5"
+                                                                class="forgot-password">{{ __('Forgot Password?') }}</a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                        <form id="register-form" action="#" method="post">
+                                            <div class="form-group">
+                                                <input type="text" name="username" id="username"
+                                                    tabindex="1" class="form-control"
+                                                    placeholder="{{ __('Username') }}" value="">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="email" name="email" id="email"
+                                                    tabindex="1" class="form-control"
+                                                    placeholder="{{ __('Email Address') }}" value="">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" name="password" id="password2"
+                                                    tabindex="2" class="form-control"
+                                                    placeholder="{{ __('Password') }}">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" name="confirm-password"
+                                                    id="confirm-password" tabindex="2" class="form-control"
+                                                    placeholder="{{ __('Confirm Password') }}">
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="row">
+                                                    <div class="col-sm-6 col-sm-offset-3">
+                                                        <input type="submit" name="register-submit"
+                                                            id="register-submit" tabindex="4"
+                                                            class="form-control btn btn-register"
+                                                            value="{{ __('Register Now') }}">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         @extends('layout.footer')
         <!-- =====  CONTAINER END  ===== -->
